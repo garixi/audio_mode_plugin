a9593591986dc80fdc0831366764f02e01e9805c
Giovanni Arixi <giovanni.arixi@softfobia.com>
2025-05-05 17:20:49 +0200

    feat: upgrade to sdk35

--- android/src/main/kotlin/com/softfobia/audio_mode_plugin/AudioModePlugin.kt
+++ android/src/main/kotlin/com/softfobia/audio_mode_plugin/AudioModePlugin.kt
@@ -13,25 +13,13 @@ class AudioModePlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
     private lateinit var channel: MethodChannel
     private lateinit var audioManager: AudioManager
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         context = flutterPluginBinding.applicationContext
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "audio_mode_plugin")
         channel.setMethodCallHandler(this)
         audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
     }
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: io.flutter.plugin.common.PluginRegistry.Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "audio_mode_plugin")
-            val plugin = AudioModePlugin()
-            plugin.context = registrar.context()
-            plugin.audioManager = registrar.context().getSystemService(Context.AUDIO_SERVICE) as AudioManager
-            channel.setMethodCallHandler(plugin)
-        }
-    }
-
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
             "setAudioModeToSpeaker" -> {
                 audioManager.isSpeakerphoneOn = true
@@ -44,8 +32,7 @@ class AudioModePlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
             else -> result.notImplemented()
         }
     }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 }
--- test/audio_mode_plugin_test.dart
+++ test/audio_mode_plugin_test.dart
@@ -4,12 +4,32 @@ import 'package:audio_mode_plugin/audio_mode_plugin_platform_interface.dart';
 import 'package:audio_mode_plugin/audio_mode_plugin_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
+import '../lib/audio_mode_plugin_platform_interface.dart';
+
 class MockAudioModePluginPlatform
     with MockPlatformInterfaceMixin
     implements AudioModePluginPlatform {
 
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<void> configureAVAudioSession() {
+    // TODO: implement configureAVAudioSession
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setAudioModeToEarpiece() {
+    // TODO: implement setAudioModeToEarpiece
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setAudioModeToSpeaker() {
+    // TODO: implement setAudioModeToSpeaker
+    throw UnimplementedError();
+  }
 }
 
 void main() {
